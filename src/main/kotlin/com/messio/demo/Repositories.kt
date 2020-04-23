2369dafdb721325d00ce4c4be8f5dc532850999b
Jean-Pierre <jpcuvelliez@gmail.com>
2020-04-23 09:31:23 +0200

    .

--- src/main/kotlin/com/messio/demo/Repositories.kt
+++ src/main/kotlin/com/messio/demo/Repositories.kt
@@ -5,34 +5,9 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
-import org.springframework.transaction.annotation.Transactional
 import java.time.LocalTime
 
 val logger = LoggerFactory.getLogger("com.messio.demo.repositories")
-
-@Repository
-interface BankRepository: CrudRepository<Bank, Long> {
-    fun findByName(name: String): Bank?
-}
-
-@Repository
-interface CurrencyGroupRepository: CrudRepository<CurrencyGroup, Long>{
-    fun findAllByOrderByPriority(): Iterable<CurrencyGroup>
-}
-
-@Repository
-interface CurrencyRepository: CrudRepository<Currency, Long> {
-    fun findByBank(bank: Bank): Iterable<Currency>
-}
-
-@Repository
-interface InstructionRepository: CrudRepository<Instruction, Long> {
-    fun findAllByBank(bank: Bank): Iterable<Instruction>
-    fun findAllByBankAndTypeAndBookedIsNull(bank: Bank, type: InstructionType): Iterable<Instruction>
-    fun findAllByBankAndTypeAndBookedIsNotNull(bank: Bank, type: InstructionType): Iterable<Instruction>
-    @Query("select max(i.bookId) from Instruction i")
-    fun findMaxBookId(): Long?
-}
 
 @Component
 class Facade(
@@ -40,8 +15,8 @@ class Facade(
         val currencyGroupRepository: CurrencyGroupRepository,
         val currencyRepository: CurrencyRepository,
         val instructionRepository: InstructionRepository
-){
-    fun book(instruction: Instruction, time: LocalTime){
+) {
+    fun book(instruction: Instruction, time: LocalTime) {
         val maxBookId = instructionRepository.findMaxBookId() ?: 0L
         instructionRepository.findById(instruction.id).ifPresent {
             logger.debug("Booking @ $time: $instruction")
@@ -51,3 +26,29 @@ class Facade(
         }
     }
 }
+
+@Repository
+interface BankRepository : CrudRepository<Bank, Long> {
+    fun findByName(name: String): Bank?
+}
+
+@Repository
+interface CurrencyGroupRepository : CrudRepository<CurrencyGroup, Long> {
+    fun findAllByOrderByPriority(): Iterable<CurrencyGroup>
+}
+
+@Repository
+interface CurrencyRepository : CrudRepository<Currency, Long> {
+    fun findByBank(bank: Bank): Iterable<Currency>
+}
+
+@Repository
+interface InstructionRepository : CrudRepository<Instruction, Long> {
+    fun findAllByBank(bank: Bank): Iterable<Instruction>
+    fun findAllByBankAndTypeAndBookedIsNull(bank: Bank, type: InstructionType): Iterable<Instruction>
+    fun findAllByBankAndTypeAndBookedIsNotNull(bank: Bank, type: InstructionType): Iterable<Instruction>
+
+    @Query("select max(i.bookId) from Instruction i")
+    fun findMaxBookId(): Long?
+}
+
