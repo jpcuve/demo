bef41c54066e5fbfdcaaa7dff936da3220a9c973
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2020-04-30 22:15:24 +0200

    .

--- src/main/kotlin/com/messio/demo/AuthenticationController.kt
+++ src/main/kotlin/com/messio/demo/AuthenticationController.kt
@@ -1,16 +1,38 @@
 package com.messio.demo
 
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin
 class AuthenticationController {
     @PostMapping("/sign-in")
-    fun apiSignIn(signInValue: SignInValue): User? {
+    fun apiSignIn(@RequestBody signInValue: SignInValue): User? {
         return null
+    }
+
+    @GetMapping("/sign-out")
+    fun apiSignOut(): String {
+        return "ok"
+    }
+
+    @PostMapping("/sign-up")
+    fun apiSignUp(@RequestBody signUpValue: SignUpValue): String {
+        return "ok"
+    }
+
+    @PostMapping("/update-password")
+    fun apiUpdatePassword(@RequestBody updatePasswordValue: UpdatePasswordValue): String {
+        return "ok"
+    }
+
+    @PostMapping("/reset-password")
+    fun apiResetPassword(@RequestBody resetPasswordValue: ResetPasswordValue): String {
+        return "ok"
+    }
+
+    @PostMapping("/google-sign-in")
+    fun apiGoogleSignIn(@RequestBody googleSignInValue: GoogleSignInValue): String {
+        return "ok"
     }
 }
--- src/main/kotlin/com/messio/demo/DataTransfer.kt
+++ src/main/kotlin/com/messio/demo/DataTransfer.kt
@@ -1,4 +1,7 @@
 package com.messio.demo
 
-open class ResetPasswordValue(open var email: String?)
-class SignInValue(override var email: String?, var password: String?): ResetPasswordValue(email)
+data class SignInValue(val email: String = "", val password: String = "")
+data class GoogleSignInValue(val token: String = "")
+data class SignUpValue(val email: String = "", val password: String = "", val name: String = "")
+data class UpdatePasswordValue(val newPassword: String = "", val newPasswordConfirmation: String = "", val token: String = "")
+data class ResetPasswordValue(val email: String = "")
