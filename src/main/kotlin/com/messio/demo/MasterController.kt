ed13029cdb7f9d02346174425f187e62367933b7
Jean-Pierre <jpcuvelliez@gmail.com>
2020-04-30 10:13:29 +0200

    .

--- src/main/kotlin/com/messio/demo/MasterController.kt
+++ src/main/kotlin/com/messio/demo/MasterController.kt
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.math.BigDecimal
-import java.time.LocalTime
 
 @RestController
 @RequestMapping("/master")
