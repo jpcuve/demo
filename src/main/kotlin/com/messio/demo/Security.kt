4849751c9d4333fc495ea2b3563b2259f9681ac3
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2020-05-01 22:52:11 +0200

    .

--- src/main/kotlin/com/messio/demo/Security.kt
+++ src/main/kotlin/com/messio/demo/Security.kt
@@ -4,14 +4,17 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationProvider
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.Authentication
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter
 import java.security.SecureRandom
+import javax.servlet.Filter
+import javax.servlet.http.HttpServletRequest
 
 
 @Configuration
@@ -19,34 +22,46 @@ class SecurityConfiguration(val facade: Facade) : WebSecurityConfigurerAdapter()
     private val logger: Logger = LoggerFactory.getLogger(SecurityConfiguration::class.java)
 
     override fun configure(http: HttpSecurity) {
-        http.authorizeRequests()
-                // .antMatchers("/h2-console").hasAuthority("ADMIN")
+        http
+                .csrf().disable()
+                .addFilter(preAuthTokenHeaderFilter())
+                .authorizeRequests()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .headers().frameOptions().disable()
-                .and()
-                .csrf().disable()
     }
 
     @Bean
-    fun authenticationProvider(userDetailsService: UserDetailsService?, passwordEncoder: PasswordEncoder?): AuthenticationProvider {
-        val authenticationProvider = DaoAuthenticationProvider()
-        authenticationProvider.setUserDetailsService(userDetailsService)
-        authenticationProvider.setPasswordEncoder(passwordEncoder)
-        return authenticationProvider
+    fun preAuthTokenHeaderFilter(): Filter {
+        val filter = object : AbstractPreAuthenticatedProcessingFilter() {
+            override fun getPreAuthenticatedPrincipal(request: HttpServletRequest): Any? {
+                val authorizationHeader = request.getHeader("Authorization") ?: return null
+                logger.debug("Authorization header: $authorizationHeader")
+                // here decode the JWT token, return the token contents or null
+                return UserValue(email = "text")
+            }
+
+            override fun getPreAuthenticatedCredentials(request: HttpServletRequest): Any {
+                return "N/A"
+            }
+        }
+        filter.setAuthenticationManager(authenticationManager())
+        return filter
     }
 
     @Bean
-    override fun userDetailsService(): UserDetailsService {
-        return UserDetailsService { username: String -> facade.userRepository.findTopByEmail(username) }
+    override fun authenticationManager(): AuthenticationManager {
+        return object : AuthenticationManager {
+            override fun authenticate(authentication: Authentication?): Authentication {
+                // check out if authentication ok, set the granted authorities as well, set authenticated = true
+
+                return authentication ?: throw BadCredentialsException("Not authenticated")
+            }
+        }
     }
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
+    fun passwordEncoder(): PasswordEncoder? {
         return BCryptPasswordEncoder(10, SecureRandom())
     }
 }
