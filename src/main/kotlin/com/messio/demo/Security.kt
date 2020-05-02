0d60c57ae86bd0d204d08d324c80bf52a9f46154
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2020-05-02 12:08:28 +0200

    .

--- src/main/kotlin/com/messio/demo/Security.kt
+++ src/main/kotlin/com/messio/demo/Security.kt
@@ -1,6 +1,9 @@
 package com.messio.demo
 
-import org.slf4j.Logger
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
