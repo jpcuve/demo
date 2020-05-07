c1e51be7ab30d1fc765e70392b812319a193fd2b
Jean-Pierre <jpcuvelliez@gmail.com>
2020-05-07 11:48:16 +0200

    .

--- src/main/kotlin/com/messio/demo/MasterController.kt
+++ src/main/kotlin/com/messio/demo/MasterController.kt
@@ -3,67 +3,42 @@ package com.messio.demo
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.*
-import javax.annotation.security.RolesAllowed
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/master")
 @CrossOrigin(allowCredentials = "true")
-class MasterController @Autowired constructor(
-        val appProperties: AppProperties,
-        val facade: Facade
-) {
+class MasterController @Autowired constructor(val facade: Facade) {
     private val logger: Logger = LoggerFactory.getLogger(MasterController::class.java)
 
-    init {
-        logger.debug("App prop: ${appProperties.defaultBank}")
-    }
-
     @GetMapping("/statement")
-    fun apiStatement(
-            @RequestParam("bank-name") bankName: String,
-            @RequestParam("account-name") accountName: String
-    ): List<Instruction> {
-        return facade.bankRepository.findByName(bankName)
-                ?.let { bank ->
-                    facade.instructionRepository.findAllByBank(bank)
-                            .filter { it.booked != null && (it.partyNames.contains(accountName)) }
-                            .sortedBy { it.bookId ?: 0L }
-                            .toList()
-                }
-                ?: emptyList()
+    fun apiStatement(@Autowired req: HttpServletRequest): List<Instruction> {
+        facade.userRepository.findTopByEmail(req.userPrincipal.name)?.let {
+            return facade.instructionRepository.findAllByBank(it.account.bank)
+                    .filter { i -> i.booked != null && (i.partyNames.contains(it.account.name)) }
+                    .sortedBy { i -> i.bookId ?: 0L }
+                    .toList()
+        }
+        throw CustomException("User not found")
     }
 
-    @GetMapping("/static")
-    fun apiStatic(@Autowired req: HttpServletRequest): Map<String, Any> {
-        val principal = req.userPrincipal.name
-        logger.debug("Principal: $principal")
-        facade.userRepository.findTopByEmail(principal)
-                ?.let {
-                    val currencyGroups = facade.currencyGroupRepository.findAll()
-                    val currencies = facade.currencyRepository.findByBank(it.account.bank)
-                    return mapOf(
-                            "profile" to ProfileValue(true, "", it.name, it.securityRoles),
-                            "account" to it.account,
-                            "bank" to it.account.bank,
-                            "currencyGroups" to currencyGroups,
-                            "currencies" to currencies
-                    )
-                }
-        throw CustomException("User not found: $principal")
+    @GetMapping("/perpetual")
+    fun apiPerpetual(@Autowired req: HttpServletRequest): Map<String, Any> {
+        facade.userRepository.findTopByEmail(req.userPrincipal.name)?.let {
+            val currencyGroups = facade.currencyGroupRepository.findAll()
+            val currencies = facade.currencyRepository.findByBank(it.account.bank)
+            return mapOf(
+                    "profile" to ProfileValue(true, "", it.name, it.securityRoles),
+                    "account" to it.account,
+                    "bank" to it.account.bank,
+                    "currencyGroups" to currencyGroups,
+                    "currencies" to currencies
+            )
+        }
+        throw CustomException("User not found")
     }
-
-    @GetMapping("/all-banks")
-    fun apiAllBanks() = facade.bankRepository.findAll().asSequence().toList()
-
-    @GetMapping("/all-currency-groups")
-    @RolesAllowed("ROLE_USER")
-    fun apiAllCurrencyGroups() = facade.currencyGroupRepository.findAllByOrderByPriority().asSequence().toList()
-
-    @GetMapping("/all-currencies")
-    fun apiAllCurrencies() = facade.currencyRepository.findAll().asSequence().toList()
-
-    @GetMapping("/all-instructions")
-    fun apiAllInstructions() = facade.instructionRepository.findAll().asSequence().toList()
 }
