5d3a0440b2b2f9f0f9272c2fb33900af67b85cfd
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2020-05-10 13:50:16 +0200

    .

---
+++ src/main/resources/public/precache-manifest.2ab7440b31793a8931520efbd763646a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916ceb904e49b3d01c26bfaa55600a63",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "8401a4562f069ca92287",
+    "url": "/app/static/css/main.ad78ba3c.chunk.css"
+  },
+  {
+    "revision": "ba4ae4e026d0273e05cf",
+    "url": "/app/static/js/2.e2727a2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app/static/js/2.e2727a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8401a4562f069ca92287",
+    "url": "/app/static/js/main.d8e7e53a.chunk.js"
+  },
+  {
+    "revision": "9b0bed3b78ac30ecd4d6",
+    "url": "/app/static/js/runtime-main.0d32149f.js"
+  }
+]);
